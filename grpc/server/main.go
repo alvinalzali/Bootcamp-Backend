c6219968d6076f6e16408cdda7099f195d5e5369
alvinalzali <alvinamaterasu@gmail.com>
2025-10-02 23:31:14 +0700

    updated gRPC host-client, some modification but still not working yet

--- grpc/server/main.go
+++ grpc/server/main.go
@@ -21,6 +21,12 @@ type server struct {
 	db *sql.DB
 }
 
+type user struct {
+	ID   int
+	Name string
+	Age  int
+}
+
 var db *sql.DB
 
 func (s *server) GetUserInfo(ctx context.Context, req *service.UserRequest) (*service.UserResponse,
@@ -33,22 +39,97 @@ func (s *server) GetUserInfo(ctx context.Context, req *service.UserRequest) (*se
 	// 	Age:  30,
 	// }
 
-	// Simulasi pengambilan data user dari database
-	user := &service.UserResponse{}
-	row := s.db.QueryRow("SELECT id, name, age FROM users WHERE id = $1", req.Id)
-	err := row.Scan(&user.Id, &user.Name, &user.Age)
+	// ambil data user dari request
+	id := req.Id
 
-	//cek error dan bind ke struct user
+	// query data dari request dengan id yang di dapat dan bind ke struct user
+	var u user
+	err := s.db.QueryRow("SELECT id, name, age FROM users WHERE id = $1", id).Scan(&u.ID, &u.Name, &u.Age)
 	if err != nil {
 		if err == sql.ErrNoRows {
-			return nil, fmt.Errorf("user with ID %d not found", req.Id)
+			return nil, fmt.Errorf("user with ID %d not found", id)
 		}
 		return nil, fmt.Errorf("failed to get user: %v", err)
 	}
 
-	// Kembalikan data user
+	// simpan data user
+	user := &service.UserResponse{
+		Id:   int32(u.ID),
+		Name: u.Name,
+		Age:  int32(u.Age),
+	}
+
+	// simpan context dan user ke return
+
 	return user, nil
+
+	// // Simulasi pengambilan data user dari database
+	// user := &service.UserResponse{}
+	// row := s.db.QueryRow("SELECT id, name, age FROM users WHERE id = $1", req.Id)
+	// err := row.Scan(&user.Id, &user.Name, &user.Age)
+
+	// //cek error dan bind ke struct user
+	// if err != nil {
+	// 	if err == sql.ErrNoRows {
+	// 		return nil, fmt.Errorf("user with ID %d not found", req.Id)
+	// 	}
+	// 	return nil, fmt.Errorf("failed to get user: %v", err)
+	// }
+
+	// // Kembalikan data user
+	// return user, nil
 }
+
+// func (s *server) GetUserInfo(c echo.Context, req *service.UserRequest) (*service.UserResponse, error) {
+
+// 	// Implement your logic here
+// 	// Proses untuk mendapatkan data user
+// 	// user := &service.UserResponse{
+// 	// 	Id:   req.Id,
+// 	// 	Name: "John Doe",
+// 	// 	Age:  30,
+// 	// }
+
+// 	// // ambil data user dari request
+// 	// id := req.Id
+
+// 	// // query data dari request dengan id yang di dapat dan bind ke struct user
+// 	// var u User
+// 	// err := s.db.QueryRow("SELECT id, name, age FROM users WHERE id = $1", id).Scan(&u.ID, &u.Name, &u.Age)
+// 	// if err != nil {
+// 	// 	if err == sql.ErrNoRows {
+// 	// 		return c.JSON(http.StatusNotFound, echo.Map{
+// 	// 			"error": fmt.Sprintf("User with ID %d not found", id),
+// 	// 		})
+// 	// 	}
+// 	// 	return c.JSON(http.StatusInternalServerError, echo.Map{
+// 	// 		"error": err.Error(),
+// 	// 	})
+// 	// }
+// 	// // simpan context dan user ke return
+
+// 	// return c.JSON(http.StatusNotFound, echo.Map{
+// 	// 	"id":   u.ID,
+// 	// 	"name": u.Name,
+// 	// 	"age":  u.Age,
+// 	// })
+
+// 	// Simulasi pengambilan data user dari database
+// 	user := &service.UserResponse{}
+// 	row := s.db.QueryRow("SELECT id, name, age FROM users WHERE id = $1", req.Id)
+// 	err := row.Scan(&user.Id, &user.Name, &user.Age)
+
+// 	//cek error dan bind ke struct user
+// 	if err != nil {
+// 		if err == sql.ErrNoRows {
+// 			return nil, fmt.Errorf("user with ID %d not found", req.Id)
+// 		}
+// 		return nil, fmt.Errorf("failed to get user: %v", err)
+// 	}
+
+// 	// Kembalikan data user
+// 	return user, nil
+// }
 
 func main() {
 
